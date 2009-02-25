e8495a249a4f9d51ca59dd85e92aacb246246fa7
acrollet <acrollet@ba5f90f1-0254-44b5-bdf0-a77a308f5e5b>
2009-02-25 01:41:42 +0000

    initial import

---
+++ Vimafarian.user.js
@@ -0,0 +1,65 @@
+// ==UserScript==
+// @name          Vimafarian
+// @author	      Adrian Rollett
+// @namespace     ACR
+// @description   vimperator clone for Safari/GreaseKit
+// @exclude       http://gmail.com/*
+// @include       *
+// ==/UserScript==
+//
+// CREDITS
+// http://www.openjs.com/scripts/events/keyboard_shortcuts/
+//
+
+// functions 
+function goToBottom(){
+  var sh=document.body.scrollHeight;
+  var oh=document.body.offsetHeight;
+  if (sh > oh) window.scrollTo(0,sh);
+  else window.scrollTo(0,oh);
+}
+
+document.addEventListener('keypress', 
+	function(e){
+
+    var element;
+    if(e.target) element=e.target;
+    else if(e.srcElement) element=e.srcElement;
+    if(element.nodeType==3) element=element.parentNode;
+
+    if(element.tagName == 'INPUT' || element.tagName == 'TEXTAREA') return;
+
+		var keyNum;
+
+    if (e.which) {
+			keyNum = e.which;
+		}
+
+		var keyChar = String.fromCharCode(keyNum);
+
+    // Movement functions
+    // scroll to bottom
+    else if (keyChar == 'G') {
+      goToBottom();
+    }
+    // scroll up
+		if (keyChar == 'j') {
+			window.scrollBy(0,50);
+		}
+    // scroll down
+		else if (keyChar == 'k') {
+      window.scrollBy(0,-50);
+		}
+
+    // Browser functions
+    // close window
+		else if (keyChar == 'd') {
+      window.open('', '_self', '');
+      top.window.close();
+    }
+    // else if (keyChar == 'r') {
+      // window.location.reload();
+    // }
+
+	}, 
+true);
